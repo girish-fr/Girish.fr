fdd490bb2e28cfe2045efd8b84da2daef148d728
DWE-CLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2024-10-15 17:09:20 +0530

    Fix : fixed text

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -120,11 +120,7 @@ const Works = () => {
           <motion.p
             className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
           >
-            Following projects showcases my skills and experience through
-            real-world examples of my work. Each project is briefly described with
-            links to code repositories and live demos in it. It reflects my
-            ability to solve complex problems, work with different technologies,
-            and manage projects effectively.
+            I am currently focused on a project aimed at making Perplexity AI’s 1-year subscription more accessible to users who may not have the financial means to afford it. By developing cost-effective solutions and exploring the most affordable options, my goal is to ensure that even those with limited resources can benefit from the powerful capabilities of Perplexity AI. This initiative reflects my commitment to bridging the gap between cutting-edge technology and users in need, making advanced tools available to a wider audience.
           </motion.p>
           <Image src={color_sharp} alt="color-sharp" className="absolute z-[-1] h-80 -left-60 w-screen -top-20" />
         </div>
