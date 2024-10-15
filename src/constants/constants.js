e10a2ecfea167ec94286720ac1717d43852c1a4e
DWE-CLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2024-10-15 16:27:26 +0530

    feat : Files added

---
+++ src/constants/constants.js
@@ -0,0 +1,268 @@
+import {
+  mobile,
+  backend,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  carrent,
+  jobit,
+  inkSync,
+  tracker,
+  threejs,
+  nextjs,
+  express,
+  java,
+  jwt,
+  nextauth,
+  socialSphere,
+} from "../assets";
+import perpex from "../assets//perplex.jpg";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Skilled Cracker",
+    icon: web,
+  },
+  {
+    title: "Experienced Carder",
+    icon: mobile,
+  },
+  {
+    title: "Efficient Designer",
+    icon: backend,
+  },
+  // {
+  //   title: "Creator",
+  //   icon: creator,
+  // },
+];
+
+const technologies = [
+  {
+    name: "React JS",
+    icon: reactjs,
+    position: [0, 0, 0],
+  },
+  {
+    name: "Next JS",
+    icon: nextjs,
+    position: [2, 0, 0],
+  },
+  {
+    name: "Express JS",
+    icon: express,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "JWT",
+    icon: jwt,
+  },
+  // {
+
+  //   name: "NextAuth",
+  //   icon: nextauth,
+  // },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const projects = [
+  {
+    name: "Perplexity",
+    description:
+      "I’m offering a 1-year Perplexity subscription (worth $200) for 749 INR ($10) via email, making it affordable for users who need it.",
+    features: [
+      "I am currently focused on a project aimed at making Perplexity AI’s 1-year subscription more accessible to users who may not have the financial means to afford it.",
+      "Users can draw on the whiteboard and the changes will be reflected in real-time to all the users in the room.",
+    ],
+    tags: [
+      {
+        name: "Perplexity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Ai",
+        color: "green-text-gradient",
+      },
+      {
+        name: "GPT",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Research",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "Cheapest",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: perpex,
+    source_code_link: "https://t.me/girishfr",
+    live_link: "https://t.me/girishfr",
+  },
+];
+
+// const experiences = [
+//   {
+//     title: "React.js Developer",
+//     company_name: "Starbucks",
+//     icon: starbucks,
+//     iconBg: "#383E56",
+//     date: "March 2020 - April 2021",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "React Native Developer",
+//     company_name: "Tesla",
+//     icon: tesla,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2021 - Feb 2022",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Web Developer",
+//     company_name: "Shopify",
+//     icon: shopify,
+//     iconBg: "#383E56",
+//     date: "Jan 2022 - Jan 2023",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Full stack Developer",
+//     company_name: "Meta",
+//     icon: meta,
+//     iconBg: "#E6DEDD",
+//     date: "Jan 2023 - Present",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+// ];
+
+// const testimonials = [
+//   {
+//     testimonial:
+//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+//     name: "Sara Lee",
+//     designation: "CFO",
+//     company: "Acme Co",
+//     image: "https://randomuser.me/api/portraits/women/4.jpg",
+//   },
+//   {
+//     testimonial:
+//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
+//     name: "Chris Brown",
+//     designation: "COO",
+//     company: "DEF Corp",
+//     image: "https://randomuser.me/api/portraits/men/5.jpg",
+//   },
+//   {
+//     testimonial:
+//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//     name: "Lisa Wang",
+//     designation: "CTO",
+//     company: "456 Enterprises",
+//     image: "https://randomuser.me/api/portraits/women/6.jpg",
+//   },
+// ];
+
+export {
+  services,
+  technologies,
+  //  experiences,
+  //  testimonials,
+  projects,
+};
