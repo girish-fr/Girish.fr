c8d81c903b92338a46b79cec2c7e8ff95b78d7f7
DWE-CLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2024-10-15 17:29:26 +0530

    Fix : fixed text - 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
 'use client'
 import { motion } from "framer-motion";
-
 import Image from "next/image";
-import heroImg from "../assets/user.svg"
+import heroImg from "../assets/user.svg";
 import { gsap } from "gsap";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const Hero = () => {
@@ -12,7 +11,8 @@ const Hero = () => {
   const secondText = useRef(null);
   const slider = useRef(null);
 
-  let xPercent = 0;
+  let xPercent1 = 0;
+  let xPercent2 = -100;
   let direction = -1;
 
   useEffect(() => {
@@ -23,71 +23,89 @@ const Hero = () => {
         scrub: 0.5,
         start: 0,
         end: window.innerHeight,
-        onUpdate: e => direction = e.direction * -1
+        onUpdate: (e) => (direction = e.direction * -1),
       },
       x: "-500px",
-    })
+    });
     requestAnimationFrame(animate);
   }, []);
 
   const animate = () => {
-    if (xPercent < -100) {
-      xPercent = 0;
+    if (xPercent1 < -100) {
+      xPercent1 = 0;
+    } else if (xPercent1 > 0) {
+      xPercent1 = -100;
     }
-    else if (xPercent > 0) {
-      xPercent = -100;
+
+    if (xPercent2 < -100) {
+      xPercent2 = 0;
+    } else if (xPercent2 > 0) {
+      xPercent2 = -100;
     }
-    gsap.set(firstText.current, { xPercent: xPercent })
-    gsap.set(secondText.current, { xPercent: xPercent })
+
+    gsap.set(firstText.current, { xPercent: xPercent1 });
+    gsap.set(secondText.current, { xPercent: xPercent2 });
+
     requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  }
+
+    xPercent1 += 0.1 * direction;
+    xPercent2 += 0.09 * direction;
+  };
 
   return (
-    <section className={`relative z-[-1] w-full h-screen mx-auto banner overflow-hidden`}>
-      <div
-        className={`absolute inset-0 top-[150px] right-40 z-[-1] animation  max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5`}
-      >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
-
-        <div data-scroll data-scroll-speed='0.4'>
-          <h1 className={`font-black lg:text-[90px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-20 text-white`}>
-            Hi, I'm <p className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 text-[60px] lg:text-[110px]'>Girish</p>
-          </h1>
-
-        </div>
-      </div>
-      <div className="sliderContainer md:z-[-10]">
-        <div ref={slider} className="slider overflow-hidden text-secondary text-[70px] lg:text-[200px]">
-          <p ref={firstText} >Fast Learner .</p>
-          <p ref={secondText}>Idealistic Person</p>
-        </div>
-      </div>
-
-
-      <Image loading="lazy" src={heroImg} alt="hero" className="z-[-2] absolute bottom-24 right-0 animation" />
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-30 cursor-pointer'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 cursor-pointer'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
+      <section className={`relative z-[-1] w-full h-screen mx-auto banner overflow-hidden`}>
+        <div
+            className={`absolute inset-0 top-[150px] right-40 z-[-1] animation max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5`}
+        >
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
           </div>
-        </a>
-      </div>
-    </section>
+
+          <div data-scroll data-scroll-speed="0.4">
+            <h1
+                className={`font-black lg:text-[90px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-20 text-white`}
+            >
+              Hi, I'm{" "}
+              <p className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 text-[60px] lg:text-[110px]">
+                Girish
+              </p>
+            </h1>
+          </div>
+        </div>
+
+        <div className="sliderContainer md:z-[-10]">
+          <div ref={slider} className="slider overflow-hidden text-secondary text-[70px] lg:text-[200px]">
+            <p ref={firstText}>Fast Learner .</p>
+            <p ref={secondText}>Idealistic Person</p>
+          </div>
+        </div>
+
+        <Image
+            loading="lazy"
+            src={heroImg}
+            alt="hero"
+            className="z-[-2] absolute bottom-24 right-0 animation"
+        />
+
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-30 cursor-pointer">
+          <a href="#about">
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 cursor-pointer">
+              <motion.div
+                  animate={{
+                    y: [0, 24, 0],
+                  }}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }}
+                  className="w-3 h-3 rounded-full bg-secondary mb-1"
+              />
+            </div>
+          </a>
+        </div>
+      </section>
   );
 };
 
